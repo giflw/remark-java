276622f4bcf69fba53100f9d2a70927af8b58368
Phil DeJarnett <phil@overzealous.com>
2011-10-05 05:52:12 -0400

    updated gradle build script, added package-level docs.

---
+++ src/main/java/com/overzealous/remark/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2011 OverZealous Creations, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The root package for the OverZealous Creations, LLC, Remark library.
+ *
+ * This library is used to convert HTML files back into valid Markdown code.
+ *
+ * For more information, see <a href="https://bitbucket.org/OverZealous/remark">the BitBucket Repository</a>.
+ *
+ * @since 1.0
+ */
+package com.overzealous.remark;
